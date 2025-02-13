3a9e32201ed4851a6feff988b3dd1efa2aee40fc
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-13 15:29:07 -0300

    mudança enunciado

--- src/controller/fatcontroller.java
+++ src/controller/fatcontroller.java
@@ -1,11 +1,7 @@
 package controller;
 
 public class fatcontroller {
-	/*3. Crie uma função recursiva que exiba o resultado do fatorial de um número (Pela limitação da
-	recursividade, o número de entrada deverá ser baixo para não dar estouro(limite de entrada = 12)):
-	O código deve trazer como comentários:
-	A condição de parada
-	Como escrever a função para o termo n em função do termo anterior*/
+	
 	
 	public fatcontroller(){
 		super();
--- src/view/main.java
+++ src/view/main.java
@@ -4,6 +4,12 @@ import javax.swing.JOptionPane;
 import controller.fatcontroller;
 
 public class main {
+	/*3. Crie uma função recursiva que exiba o resultado do fatorial de um número (Pela limitação da
+	recursividade, o número de entrada deverá ser baixo para não dar estouro(limite de entrada = 12)):
+	O código deve trazer como comentários:
+	A condição de parada
+	Como escrever a função para o termo n em função do termo anterior*/
+	
 	public static void main(String[] args) {
 		int n;
 		n = Integer.parseInt(JOptionPane.showInputDialog("Insira um número entre 1 e 12."));
